c2a1096e3baaa2a3d3682ccfeace75723728cd4f
Andrey Strokan <andrey.strokan@saritasa.com>
2024-08-15 12:36:29 +0500

    Remove unused module JC-646

--- apps/angular/src/app/app.component.ts
+++ apps/angular/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 /** App component. */
 @Component({
 	selector: 'camp-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
+	standalone: true,
+	imports: [AppRoutingModule],
 })
 export class AppComponent {}
