34e061b4ccddbf9f34eb7acc4bf9076b5c1c4507
Andrey Strokan <andrey.strokan@saritasa.com>
2024-08-15 16:10:22 +0500

    Install material design JC-646

--- apps/angular/src/main.ts
+++ apps/angular/src/main.ts
@@ -5,11 +5,12 @@ import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { routes } from './app/app-routing.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 if (environment.production) {
 	enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-	providers: [provideRouter(routes)],
+	providers: [provideRouter(routes), provideAnimationsAsync()],
 }).catch(err => console.error(err));
