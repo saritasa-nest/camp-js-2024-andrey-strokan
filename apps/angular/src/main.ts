8b3c9b1cb55887ea77d6e7e227b61537a9c5fdce
Andrey Strokan <andrey.strokan@saritasa.com>
2024-08-15 16:11:43 +0500

    Fix lints JC-646

--- apps/angular/src/main.ts
+++ apps/angular/src/main.ts
@@ -1,11 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { routes } from './app/app-routing.module';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 if (environment.production) {
 	enableProdMode();
