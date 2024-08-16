df5b0957b04019dba694a388f3bd0c607af618a0
Andrey Strokan <andrey.strokan@saritasa.com>
2024-08-16 13:49:20 +0500

    Map dto to model JC-646

--- apps/angular/src/app/anime-dashboard/anime-dashboard.component.ts
+++ apps/angular/src/app/anime-dashboard/anime-dashboard.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 
-import { AnimeResponseDTO } from '../DTOs/AnimeResponseDTO';
-import { AnimeDTO } from '../DTOs/AnimeDTO';
+import { map } from 'rxjs/operators';
 
 import { ApiService } from '.././services/api.service';
+import { AnimeFilm } from '../entities/animeFilm';
 
 /** Anime dashboard. */
 @Component({
@@ -21,16 +21,26 @@ export class AnimeDashboardComponent implements OnInit {
 	public displayedColumns: string[] = ['imageSourceURL', 'titleEnglish', 'titleJapan', 'airedStart', 'filmType'];
 
 	/** Anime films. */
-	public animeFilms: AnimeDTO[] = [];
+	public animeFilms: AnimeFilm[] = [];
 
 	public constructor(private apiService: ApiService) {}
 
 	/** @inheritdoc */
 	public ngOnInit(): void {
 		this.apiService.getAllAnimeFilms()
+			.pipe(
+				map(response => response.results),
+				map(animeDtoArray => animeDtoArray.map(item => ({
+					imageSourceURL: item.image,
+					titleEnglish: item.title_eng,
+					titleJapan: item.title_jpn,
+					airedStart: new Date(item.aired.start),
+					filmType: item.type,
+				} as AnimeFilm))),
+			)
 			.subscribe(
-				(data: AnimeResponseDTO) => {
-					this.animeFilms = data.results;
+				animeFilms => {
+					this.animeFilms = animeFilms;
 				},
 			);
 	}
---
+++ apps/angular/src/app/entities/animeFilm.ts
@@ -0,0 +1,18 @@
+/** Anime film. */
+export type AnimeFilm = {
+
+	/** Image source. */
+	imageSourceURL: string;
+
+	/** Title english. */
+	titleEnglish: string;
+
+	/** Title Japan. */
+	titleJapan: string;
+
+	/** Aired start. */
+	airedStart: Date;
+
+	/** Film type. */
+	filmType: string;
+};
