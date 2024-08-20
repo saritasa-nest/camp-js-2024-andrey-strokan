d576b6e1dc94e4708f79973cc409fc8f264c5906
Andrey Strokan <andrey.strokan@saritasa.com>
2024-08-20 15:16:38 +0500

    Modify components JC-662
    # Conflicts:
    #	apps/angular/src/app/anime-dashboard/anime-dashboard.component.html
    #	apps/angular/src/app/anime-dashboard/anime-dashboard.component.ts

--- apps/angular/src/app/anime-dashboard/anime-dashboard.component.ts
+++ apps/angular/src/app/anime-dashboard/anime-dashboard.component.ts
@@ -1,19 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { Observable, Subscription, map } from 'rxjs';
+
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { AnimeService } from '../services/anime.service';
+import { Anime } from '../entities/anime';
+
 import { Anime } from '../entities/anime';
 
 /** Anime dashboard. */
 @Component({
 	selector: 'camp-anime-dashboard',
 	standalone: true,
-	imports: [CommonModule, MatTableModule, DatePipe],
+	imports: [
+		CommonModule,
+		MatTableModule,
+		DatePipe,
+		MatPaginatorModule,
+		MatProgressSpinnerModule,
+	],
 	templateUrl: './anime-dashboard.component.html',
 	styleUrl: './anime-dashboard.component.css',
 })
-export class AnimeDashboardComponent {
+export class AnimeDashboardComponent implements OnInit, OnDestroy {
 
 	/** Service. */
 	private readonly animeService = inject(AnimeService);
@@ -32,5 +45,57 @@ export class AnimeDashboardComponent {
 	 */
 	protected trackAnimeById(_index: number, anime: Anime): number {
 		return anime.id;
+	}
+
+	/** Visible anime. */
+	protected visibleAnime$ = new Observable<Anime[]>();
+
+	/** Count of anime. */
+	protected animeCount = 0;
+
+	/** Page size. */
+	protected pageSize = 0;
+
+	/** Current page. */
+	protected pageIndex = 0;
+
+	/** Page sizes. */
+	protected pageSizeOptions = [5, 10, 25];
+
+	/** Show page size options. */
+	protected showPageSizeOptions = true;
+
+	private subscription: Subscription = new Subscription();
+
+	/** @inheritdoc */
+	public ngOnInit(): void {
+		this.subscription = this.allAnime$.subscribe(allAnime => {
+			this.animeCount = allAnime.length;
+		});
+
+		this.pageSize = this.pageSizeOptions[0];
+		this.updateVisibleAnime();
+	}
+
+	/** @inheritdoc */
+	public ngOnDestroy(): void {
+		this.subscription.unsubscribe();
+	}
+
+	/**
+	 * Handle page event.
+	 * @param e PageEvent.
+	 */
+	protected handlePageEvent(e: PageEvent): void {
+		this.pageSize = e.pageSize;
+		this.pageIndex = e.pageIndex;
+
+		this.updateVisibleAnime();
+	}
+
+	private updateVisibleAnime(): void {
+		this.visibleAnime$ = this.allAnime$.pipe(
+			map(allAnime => allAnime.slice(this.pageIndex * this.pageSize, this.pageIndex * this.pageSize + this.pageSize)),
+		);
 	}
 }
