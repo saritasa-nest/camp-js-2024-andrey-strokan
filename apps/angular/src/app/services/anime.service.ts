4e85b1fe67cacad389c5f4e2e376d5d3df03639f
Andrey Strokan <andrey.strokan@saritasa.com>
2024-08-29 16:51:43 +0700

    Change import JC-646

--- apps/angular/src/app/services/anime.service.ts
+++ apps/angular/src/app/services/anime.service.ts
@@ -1,8 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-import { map } from 'rxjs/operators';
+import { Observable, map } from 'rxjs';
 
 import { environment } from '../../environments/environment';
 
