d1f509e8b47185ca6fa3a6df11688dca9cfbc812
Andrey Strokan <andrey.strokan@saritasa.com>
2024-09-03 11:57:37 +0700

    Modify mapper JC-646

--- apps/angular/src/app/mappers/anime.mapper.ts
+++ apps/angular/src/app/mappers/anime.mapper.ts
@@ -17,12 +17,18 @@ export class AnimeMapper implements Mapper<AnimeDto, Anime> {
 	public fromDto(dto: AnimeDto): Anime {
 		return {
 			id: dto.id,
-			imageSourceURL: dto.image,
+			created: dto.created,
+			modified: dto.modified,
 			titleEnglish: dto.title_eng,
 			titleJapanese: dto.title_jpn,
-			airedStart: new Date(dto.aired.start),
+			imageSourceURL: dto.image,
+			aired: { start: dto.aired.start, end: dto.aired.end },
 			type: dto.type,
 			status: dto.status,
+			score: dto.score,
+			userScore: dto.user_score,
+			studios: dto.studios,
+			genres: dto.genres,
 		};
 	}
 
@@ -30,16 +36,18 @@ export class AnimeMapper implements Mapper<AnimeDto, Anime> {
 	public toDto(model: Anime): AnimeDto {
 		return {
 			id: model.id,
-			created: '',
-			modified: '',
+			created: model.created,
+			modified: model.modified,
 			title_eng: model.titleEnglish,
 			title_jpn: model.titleJapanese,
 			image: model.imageSourceURL,
-			aired: { start: model.airedStart.toString() },
+			aired: { start: model.aired.start, end: model.aired.end },
 			type: model.type,
 			status: model.status,
-			studios: [],
-			genres: [],
+			score: model.score,
+			user_score: model.userScore,
+			studios: model.studios,
+			genres: model.genres,
 		};
 	}
 }
