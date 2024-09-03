d3dc450e7cb084eebb728b0bd66f83d371c489bd
Andrey Strokan <andrey.strokan@saritasa.com>
2024-09-03 11:52:42 +0700

    Modify anime entity JC-646

--- apps/angular/src/app/entities/anime.ts
+++ apps/angular/src/app/entities/anime.ts
@@ -1,14 +1,22 @@
 import { AnimeStatus } from '../enums/anime-status';
 import { AnimeType } from '../enums/anime-type';
 
+import { DateRange } from './date-range';
+
 /** Anime. */
 export type Anime = {
 
 	/** Id. */
 	readonly id: number;
 
-	/** Image source. */
-	readonly imageSourceURL: string;
+	/**
+	 * Date and time of record creation.
+	 * @example 2024-08-01
+	 */
+	readonly created: string;
+
+	/** Date and time of last edit of the entry. */
+	readonly modified: string;
 
 	/** English title. */
 	readonly titleEnglish: string;
@@ -16,8 +24,11 @@ export type Anime = {
 	/** Japanese title. */
 	readonly titleJapanese: string;
 
-	/** Aired start. */
-	readonly airedStart: Date;
+	/** Image source. */
+	readonly imageSourceURL: string;
+
+	/** When is this anime on air. */
+	readonly aired: DateRange;
 
 	/** Type. */
 	readonly type: AnimeType;
@@ -27,4 +38,16 @@ export type Anime = {
 	 * It can be Airing, Finished or Not yet aired.
 	 */
 	readonly status: AnimeStatus;
+
+	/** World score of this anime. */
+	readonly score?: number;
+
+	/** How users rated this broadcast. */
+	readonly userScore?: number;
+
+	/** Studios where the broadcast will take place. */
+	readonly studios: string[];
+
+	/** Genres. */
+	readonly genres: number[];
 };
